8763c752670d2161fef56bc1a7092cba2201dcd3
Vukknesh <leomcn@hotmail.com>
2021-10-26 17:50:59 -0300

    a

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 let api = "http://localhost:3333";
 //homologacao
-api = "http://67.207.91.188:7777";
-api = "http://localhost:8000";
+api = "http://67.207.91.188:1616";
+// api = "http://localhost:8000";
 // api = "";
 export default api;
